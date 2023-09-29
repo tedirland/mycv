0815ec2bd973806f2a912617484f6bad618cc607
tedirland <irlandth@gmail.com>
2023-09-29 06:58:14 -0500

    Created user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      // Wire up an created entities here
+      entities: [User],
       synchronize: true,
     }),
     UsersModule,
