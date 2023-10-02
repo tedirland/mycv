24b59e19cc39ff8d8e6373a4f38a373f24f523e7
tedirland <irlandth@gmail.com>
2023-10-02 12:21:12 -0500

    set up report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { User } from './users/user.entity';
       type: 'sqlite',
       database: 'db.sqlite',
       // Wire up an created entities here
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
